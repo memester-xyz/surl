5ffcf07d7bb6e7376f7d07eba79b7d6266225004
SuhailG <github@suhailg.me>
2023-01-14 03:22:08 -0700

    Update Surl.sol

--- src/Surl.sol
+++ src/Surl.sol
@@ -1,31 +1,42 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-import {Vm} from "forge-std/Vm.sol";
+import { Vm } from "forge-std/Vm.sol";
 
 library Surl {
-    Vm constant vm = Vm(address(bytes20(uint160(uint256(keccak256("hevm cheat code"))))));
+    Vm constant vm = Vm(
+        address(
+            bytes20(uint160(uint256(keccak256("hevm cheat code"))))
+        )
+    );
 
-    function get(string memory self) internal returns (uint256 status, bytes memory data) {
+    function get(string memory self)
+        internal
+        returns (uint256 status, bytes memory data)
+    {
         string[] memory empty = new string[](0);
         return get(self, empty);
     }
 
-    function get(string memory self, string[] memory headers) internal returns (uint256 status, bytes memory data) {
+    function get(string memory self, string[] memory headers)
+        internal
+        returns (uint256 status, bytes memory data)
+    {
         return curl(self, headers, "", "GET");
     }
 
-    function del(
-        string memory self
-    ) internal returns (uint256 status, bytes memory data) {
+    function del(string memory self)
+        internal
+        returns (uint256 status, bytes memory data)
+    {
         string[] memory empty = new string[](0);
         return curl(self, empty, "", "DELETE");
     }
 
-    function del(
-        string memory self,
-        string memory body
-    ) internal returns (uint256 status, bytes memory data) {
+    function del(string memory self, string memory body)
+        internal
+        returns (uint256 status, bytes memory data)
+    {
         string[] memory empty = new string[](0);
         return curl(self, empty, body, "DELETE");
     }
@@ -38,17 +49,18 @@ library Surl {
         return curl(self, headers, body, "DELETE");
     }
 
-    function patch(
-        string memory self
-    ) internal returns (uint256 status, bytes memory data) {
+    function patch(string memory self)
+        internal
+        returns (uint256 status, bytes memory data)
+    {
         string[] memory empty = new string[](0);
         return curl(self, empty, "", "PATCH");
     }
 
-    function patch(
-        string memory self,
-        string memory body
-    ) internal returns (uint256 status, bytes memory data) {
+    function patch(string memory self, string memory body)
+        internal
+        returns (uint256 status, bytes memory data)
+    {
         string[] memory empty = new string[](0);
         return curl(self, empty, body, "PATCH");
     }
@@ -61,17 +73,18 @@ library Surl {
         return curl(self, headers, body, "PATCH");
     }
 
-    function post(
-        string memory self
-    ) internal returns (uint256 status, bytes memory data) {
+    function post(string memory self)
+        internal
+        returns (uint256 status, bytes memory data)
+    {
         string[] memory empty = new string[](0);
         return curl(self, empty, "", "POST");
     }
 
-    function post(
-        string memory self,
-        string memory body
-    ) internal returns (uint256 status, bytes memory data) {
+    function post(string memory self, string memory body)
+        internal
+        returns (uint256 status, bytes memory data)
+    {
         string[] memory empty = new string[](0);
         return curl(self, empty, body, "POST");
     }
@@ -84,17 +97,18 @@ library Surl {
         return curl(self, headers, body, "POST");
     }
 
-    function put(
-        string memory self
-    ) internal returns (uint256 status, bytes memory data) {
+    function put(string memory self)
+        internal
+        returns (uint256 status, bytes memory data)
+    {
         string[] memory empty = new string[](0);
         return curl(self, empty, "", "PUT");
     }
 
-    function put(
-        string memory self,
-        string memory body
-    ) internal returns (uint256 status, bytes memory data) {
+    function put(string memory self, string memory body)
+        internal
+        returns (uint256 status, bytes memory data)
+    {
         string[] memory empty = new string[](0);
         return curl(self, empty, body, "PUT");
     }
@@ -116,19 +130,28 @@ library Surl {
         string memory curlParams = "";
 
         for (uint256 i = 0; i < headers.length; i++) {
-            curlParams = string.concat(curlParams, '-H "', headers[i], '" ');
+            curlParams =
+                string.concat(curlParams, '-H "', headers[i], '" ');
         }
 
         curlParams = string.concat(curlParams, " -X ", method, " ");
 
         if (bytes(body).length > 0) {
-            curlParams = string.concat(curlParams, ' -d "', body, '" ');
+            curlParams =
+                string.concat(curlParams, ' -d "', body, '" ');
         }
 
         string[] memory inputs = new string[](3);
         inputs[0] = "sh";
         inputs[1] = "-c";
-        inputs[2] = string(bytes.concat("./src/curl.sh ", bytes(curlParams), bytes(self), ""));
+        inputs[2] = string(
+            bytes.concat(
+                "lib/surl/src/curl.sh ",
+                bytes(curlParams),
+                bytes(self),
+                ""
+            )
+        );
         bytes memory res = vm.ffi(inputs);
 
         (status, data) = abi.decode(res, (uint256, bytes));
