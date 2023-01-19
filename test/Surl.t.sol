3a42b7fe81eb1c75a559db16bca6223d17a5dc52
SuhailG <github@suhailg.me>
2023-01-19 10:10:24 -0700

    Format

--- test/Surl.t.sol
+++ test/Surl.t.sol
@@ -15,42 +15,29 @@ contract SurlTest is Test {
     function setUp() public {}
 
     function testGet() public {
-        (
-            uint256 status,
-            bytes memory data
-        ) = "https://jsonplaceholder.typicode.com/todos/1".get();
+        (uint256 status, bytes memory data) = "https://jsonplaceholder.typicode.com/todos/1".get();
 
         assertEq(status, 200);
-        assertEq(
-            string(data),
-            '{  "userId": 1,  "id": 1,  "title": "delectus aut autem",  "completed": false}'
-        );
+        assertEq(string(data), '{  "userId": 1,  "id": 1,  "title": "delectus aut autem",  "completed": false}');
     }
 
     function testGetOptions() public {
         string[] memory headers = new string[](2);
         headers[0] = "accept: application/json";
         headers[1] = "Authorization: Basic QWxhZGRpbjpvcGVuIHNlc2FtZQ==";
-        (uint256 status, bytes memory data) = "https://httpbin.org/headers".get(
-            headers
-        );
+        (uint256 status, bytes memory data) = "https://httpbin.org/headers".get(headers);
 
         assertEq(status, 200);
 
         strings.slice memory responseText = string(data).toSlice();
-        assertTrue(
-            responseText.contains(("QWxhZGRpbjpvcGVuIHNlc2FtZQ==").toSlice())
-        );
+        assertTrue(responseText.contains(("QWxhZGRpbjpvcGVuIHNlc2FtZQ==").toSlice()));
         assertTrue(responseText.contains(("application/json").toSlice()));
     }
 
     function testPostFormData() public {
         string[] memory headers = new string[](1);
         headers[0] = "Content-Type: application/x-www-form-urlencoded";
-        (uint256 status, bytes memory data) = "https://httpbin.org/post".post(
-            headers,
-            "formfield=myemail@ethereum.org"
-        );
+        (uint256 status, bytes memory data) = "https://httpbin.org/post".post(headers, "formfield=myemail@ethereum.org");
 
         assertEq(status, 200);
 
@@ -62,10 +49,7 @@ contract SurlTest is Test {
     function testPostJson() public {
         string[] memory headers = new string[](1);
         headers[0] = "Content-Type: application/json";
-        (uint256 status, bytes memory data) = "https://httpbin.org/post".post(
-            headers,
-            '{"foo": "bar"}'
-        );
+        (uint256 status, bytes memory data) = "https://httpbin.org/post".post(headers, '{"foo": "bar"}');
 
         assertEq(status, 200);
         strings.slice memory responseText = string(data).toSlice();
@@ -74,7 +58,7 @@ contract SurlTest is Test {
     }
 
     function testPut() public {
-        (uint256 status, ) = "https://httpbin.org/put".put();
+        (uint256 status,) = "https://httpbin.org/put".put();
 
         assertEq(status, 200);
     }
@@ -82,10 +66,7 @@ contract SurlTest is Test {
     function testPutJson() public {
         string[] memory headers = new string[](1);
         headers[0] = "Content-Type: application/json";
-        (uint256 status, bytes memory data) = "https://httpbin.org/put".put(
-            headers,
-            '{"foo": "bar"}'
-        );
+        (uint256 status, bytes memory data) = "https://httpbin.org/put".put(headers, '{"foo": "bar"}');
 
         assertEq(status, 200);
         strings.slice memory responseText = string(data).toSlice();
@@ -94,13 +75,13 @@ contract SurlTest is Test {
     }
 
     function testDelete() public {
-        (uint256 status, ) = "https://httpbin.org/delete".del();
+        (uint256 status,) = "https://httpbin.org/delete".del();
 
         assertEq(status, 200);
     }
 
     function testPatch() public {
-        (uint256 status, ) = "https://httpbin.org/patch".patch();
+        (uint256 status,) = "https://httpbin.org/patch".patch();
 
         assertEq(status, 200);
     }
